bc0d9df2afffb0f96856f4dd33c43f0389920a62
Saad Ali <saad@nixknight.net>
2018-06-24 18:38:24 +0500

    Modifications for Gunicorn

--- BasicAuthManager.py
+++ BasicAuthManager.py
@@ -12,6 +12,19 @@ from email.MIMEText import MIMEText
 
 app = Flask(__name__)
 
+if os.path.exists('config.cfg'):
+  app.config.from_pyfile('config.cfg')
+  if not os.path.exists(app.config['HTPASSWD_FILE']):
+    from getpass import getpass
+    htContent = HtpasswdFile(app.config['HTPASSWD_FILE'], new=True, default_scheme='bcrypt')
+    passwordPrompt = lambda: (getpass(prompt='Enter Password for ' + app.config['ADMIN_USER'] + ": "), getpass('Confirm password: '))
+    adminPassword, adminPassword2 = passwordPrompt()
+    while adminPassword != adminPassword2:
+      print('Passwords do not match! Try again!')
+      adminPassword, adminPassword2 = passwordPrompt()
+    htContent.set_password(app.config['ADMIN_USER'], adminPassword)
+    htContent.save()
+
 def auth_required(f):
   @wraps(f)
   def authen(*args, **kwargs):
@@ -171,21 +184,3 @@ def remove_user(username):
 def custom_401(error):
     pagename = "Need Proper Authentication"
     return Response(render_template("401.html.j2", pagename=pagename), 401, {'WWWAuthenticate':'Basic realm="Login Required"'})
-
-if __name__ == '__main__':
-  if os.path.exists('config.cfg'):
-    app.config.from_pyfile('config.cfg')
-    if not os.path.exists(app.config['HTPASSWD_FILE']):
-      from getpass import getpass
-      htContent = HtpasswdFile(app.config['HTPASSWD_FILE'], new=True, default_scheme='bcrypt')
-      passwordPrompt = lambda: (getpass(prompt='Enter Password for ' + app.config['ADMIN_USER'] + ": "), getpass('Confirm password: '))
-      adminPassword, adminPassword2 = passwordPrompt()
-      while adminPassword != adminPassword2:
-        print('Passwords do not match! Try again!')
-        adminPassword, adminPassword2 = passwordPrompt()
-      htContent.set_password(app.config['ADMIN_USER'], adminPassword)
-      htContent.save()
-  else:
-    print "Create a config file 1st"
-    raise SystemExit
-  app.run()
